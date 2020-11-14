fc858fdd8a3a80f8ad7f37be1476a65bbe3e5311
marcosvoliveira <marcosvoliveira@gmail.com>
2020-11-14 10:32:55 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+//import 'package:app_3/ui/contact_page.dart';
+
+import 'package:flutter/material.dart';
+import 'ui/HomePage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
