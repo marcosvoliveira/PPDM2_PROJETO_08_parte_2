a955cf0706de7af5020e985980623a67e60fee5c
juan-patrick <juan-p.andrade@outlook.com>
2020-11-27 20:43:11 -0300

    configure the project basis

---
+++ android/app/src/main/kotlin/com/example/cpfapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contactManager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+//import 'package:contactManager/ui/contact_page.dart';
+
+import 'package:flutter/material.dart';
+import 'ui/HomePage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
